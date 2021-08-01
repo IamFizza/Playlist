dafc381113513819b6b2f9e99bf38a34bb2841cf
Fizza <87229195+IamFizza@users.noreply.github.com>
2021-08-01 15:40:23 +0500

    class 15
    class 15

---
+++ class 15/app.js
@@ -0,0 +1,18 @@
+//alert("hello");
+//for loop:
+for (var a = 0; a < 10; a++){
+    console.log(a);
+}
+//while loop:
+var b = 1;
+while(b<=10){
+    console.log(b);
+    b++;
+}
+//do while loop:
+var c = 2;
+do{
+    console.log(c);
+    c++;
+}
+while(c<12);
