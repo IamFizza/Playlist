ab6c48cfc887a870b47451ad7be23682914b2664
Fizza <87229195+IamFizza@users.noreply.github.com>
2021-08-04 17:18:08 +0500

    class 16.1 revision
    class 16.1 revision

---
+++ class 16.1 revise/app.js
@@ -0,0 +1,79 @@
+// alert("hello")
+// function setValue(){
+//     var name = document.getElementById("name");
+//     name.value = "fiza";
+// }
+
+
+// function setValue(){
+//     var paragraph= document.getElementById("para");
+//     paragraph.innerHTML= "this is example paragraph for setting para value through js"
+// }
+
+
+
+
+// function bigImg(){
+//     var img = document.getElementById("sparrow-img");
+//     img.className = 'big';
+// }
+
+
+
+// function smallImg(){
+//     var img1 = document.getElementById("sparrow-img");
+//     img1.className = 'small';
+// }
+
+
+// function addClass(){
+//     var pera = document.getElementById("para");
+//     pera.className= "bold";
+// }
+//now add multiple classes:
+// function addClass(){
+//     var pera = document.getElementById("para");
+//     pera.className += " bold font back align";
+// }
+
+// function makeBig(){
+//     var big = document.getElementById("style").style.fontSize = "5em";
+// }
+
+// function makeSmall(){
+//     var small = document.getElementById("style").style.fontSize = "0.5em";
+// }
+
+// function getElement(){
+//     var element = document.getElementsByTagName('p');
+//      console.log(element);
+
+
+// }
+// getElement();
+
+
+
+// function getElement(){
+//     var element = document.getElementsByTagName('p');
+//     var textInMiddleParagraph = element[1].innerHTML
+    
+//     element[1].innerHTML = "this is inner html"
+//     console.log(element[1].innerHTML);
+
+// }
+// getElement();
+
+// function onBulb(){
+//     var on = document.getElementById('bulb');
+//     on.src = "onbulb.jpg"
+// }
+
+// function offBulb(){
+//     var off = document.getElementById('bulb');
+//     off.src = "offbulb.jpg"
+// }
+
+
+
+
