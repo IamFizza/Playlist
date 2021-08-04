ecce11a5c19aac4c318593a653edfb4d96e830d3
Fizza <87229195+IamFizza@users.noreply.github.com>
2021-08-04 17:19:09 +0500

    class 16
    class 16

---
+++ class 16/app.js
@@ -0,0 +1,21 @@
+// alert("hello")
+//.value is for input field and .innerHTML is for p,,div etc
+// function readMore(){
+//     var text = document.getElementById('text').innerHTML;
+//     var paragraph= document.getElementById('para');
+//     paragraph.innerHTML=text;
+
+// }
+// function readMore(){
+//     var text = '<ul><li>List 1</li> <li>List 2</li> <li>List 3</li> <li>List 4</li></ul>'
+//     var paragraph= document.getElementById('para');
+//     paragraph.innerHTML=text;
+
+// }
+
+
+function big(){
+   var img = document.getElementById('sparrow')
+   img.className = 'hidden'
+}
+
